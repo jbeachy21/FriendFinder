fb65eb21c303b33ecb8133b95c089fa3797d63ed
Jasper Beachy <jbeachy@gustavus.edu>
2019-03-14 16:46:40 -0500

    modal created

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,15 +1,15 @@
 var friendsData = require("../app/data/friends.js");
-
+var path = require("path");
 
 module.exports = function(app) {
 
 
 
-app.get("/api/survey", function(req, res) {
+app.get("/api/friends", function(req, res) {
     
     //This will be used to display a JSON of all possible friends.
 
-    res.sendFile(path.join(__dirname + '../public/survey.html'));
+    res.sendFile(path.join(__dirname, "../app/data/friends.js"));
     
   
   });
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var path = require("path");
+ 
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -8,8 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
 
 
 app.listen(PORT, function() {
