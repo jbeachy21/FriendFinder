cfb861c4471d6661b63c187bd7ffc94bc920d9fb
Jasper Beachy <jbeachy@gustavus.edu>
2019-03-22 17:14:48 -0500

    progress made

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,5 +1,6 @@
-var friendsData = require("../app/data/friends.js");
 var path = require("path");
+var friends = require("../app/data/friends");
+//var answers = require("../public/survey");
 
 module.exports = function(app) {
 
@@ -7,11 +8,33 @@ module.exports = function(app) {
 
 app.get("/api/friends", function(req, res) {
     
-    //This will be used to display a JSON of all possible friends.
 
     res.sendFile(path.join(__dirname, "../app/data/friends.js"));
     
   
   });
+
+  app.post("/api/friends", function(req, res) {
+        
+    var difference = [];
+    var TotalDifference = [];
+    for (var i = 0; i<friends.friends.length; i++) {
+        for (var j = 0; j<10; j++) {
+            difference[j] = friends.friends[friends.friends.length - 1] - friends.friends[i].scores[j];
+        }
+        //Gets difference of array and takes absolute value of it
+        TotalDifference[i] = Math.abs(difference.reduce());
+        console.log("TotalDifference[i]: " + TotalDifference[i]);
+    }
+    var index;
+    for (var i = 0; i<TotalDifference.length; i++) {
+      if (TotalDifference[i] < TotalDifference[i+1]) {
+        index = TotalDifference[i];
+      }
+    }
+    document.getElementById("friend").innerHTML = friends.friends[index].name;
+    document.getElementById("photoFrame").innerHTML = friends.friends[index].photo;
+
+  })
 
 }
